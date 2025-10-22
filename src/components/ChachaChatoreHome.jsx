3325f92545b70536380109eb3e80831d879af22a
Kathan Sahu <kathan.sahu2023@gmail.com>
2025-10-23 00:16:37 +0530

    Implemented Lenis and Hero

--- src/components/ChachaChatoreHome.jsx
+++ src/components/ChachaChatoreHome.jsx
@@ -1,8 +1,15 @@
 import React from 'react'
+import NavBarc from './NavBarc'
+import HeroSection from './HeroSection'
+import AboutSection from './AboutSection'
 
 const ChachaChatoreHome = () => {
   return (
-    <div>Chacha Page</div>
+    <div className='bg-slate-700  py-1'>
+      <NavBarc/>
+      <HeroSection/>
+      <AboutSection/>
+    </div>
   )
 }
 
